a2e1c2649558c2b9e208bd8a092ee262aae68f8c
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 11:25:46 +0530

    new chnages

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
     echo "given $NUMBER is greatger than 20"
-elif [$NUMBER -eq 20]; then 
+elif [ $NUMBER -eq 20 ]; then 
     echo "given $NUMBER is equals ton 20"
 else 
     echo "given $NUMBER is less ton 20"
