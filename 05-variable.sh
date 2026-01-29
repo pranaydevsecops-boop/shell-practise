e525b04ad0670c6a39ebc18b80a0ccbd5288f6ac
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:10:12 +0530

    new file added

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 : hi how are you $PERSON2"
+echo "$PERSON2 : hi $PERSON1 how are you"
+echo "$PERSON1 : I'm doing good $PERSON2"
+echo "$PERSON2 : how about BF $PERSON1"
+echo "$PERSON1 : Done $PERSON1, What about you"
