6ec154bfb1314e9e04f9d20e1eba1a1d00bdeae7
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 11:24:31 +0530

    new chnages

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-NUMBER = $1
+NUMBER=$1
 
-if [NUMBER -gt 20]; then
+if [$NUMBER -gt 20]; then
     echo "given $NUMBER is greatger than 20"
-elif [NUMBER -eq 20]; then 
+elif [$NUMBER -eq 20]; then 
     echo "given $NUMBER is equals ton 20"
 else 
     echo "given $NUMBER is less ton 20"
