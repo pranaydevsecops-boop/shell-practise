34bc66cb7c3e89774d7a3e560b4f3fde35c376b1
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:16:23 +0530

    new chnages

--- 06-variable.sh
+++ 06-variable.sh
@@ -3,12 +3,12 @@
 
 echo "please enter the user_name::"
 
-READ USER_NAME
+read USER_NAME
 
 echo "the username is $USER_NAME"
 
 echo "please enter pass::"
 
-READ -s PASSWORD
+read -s PASSWORD
 
 echo "the pass is $PASSWORD"
