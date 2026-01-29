29576d259f4ceda89c33c78a4a22f47ec6062238
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:13:34 +0530

    new file added

---
+++ 06-variable.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+echo "please enter the user_name"
+
+READ USER_NAME
+
+echo "the username is $USER_NAME"
+
+echo "please enter pass"
+
+READ -s PASSWORD
+
+echo "the pass is $PASSWORD"
