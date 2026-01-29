6f5e85e33bbc7fc2ac2ad58479f5cdd72a2e5606
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:31:11 +0530

    new chnages

--- 07-variable.sh
+++ 07-variable.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
-echo "the time is $TIMESTAMP"
+echo "the time is $START_TIME"
+
+sleep 10 
+
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed in :: $TOTAL_TIME"
