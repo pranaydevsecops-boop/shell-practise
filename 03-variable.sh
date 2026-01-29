521f940f425337eaa87b1aea18bc9986845e93da
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 09:57:54 +0530

    variable

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Ramesh : hi how are you Suresh"
+echo "Suresh : hi Ramesh how are you"
+echo "Ramesh : I'm doing good Suresh"
+echo "Suresh : how about BF Ramesh"
+echo "Ramesh : Done Ramesh, What about you"
