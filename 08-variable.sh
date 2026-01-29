8775fc4aead1202aec44b606044fdcbae6216b6f
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 11:01:39 +0530

    new chnages

---
+++ 08-variable.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+## sepecial variable 
+
+echo "all args passed to script :: $@"
+echo "no varaible passed :: $#"
+echo "present working directory :: $PWD"
+echo "script name :: "$0"
+echo "who is running :: "$USER"
---
+++ 09-datatypes.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+NUM1=20
+NUM2=20
+
+SUM=$(NUM1+NUM2)
+
+echo "sum is $SUM"
