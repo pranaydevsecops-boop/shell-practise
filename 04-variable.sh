1e684a39a664b78536208da71dfccc6d0fb74323
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:07:29 +0530

    chnages

--- 04-variable.sh
+++ 04-variable.sh
@@ -5,7 +5,7 @@ PERSON2=Suresh
 
 
 echo "$PERSON1 : hi how are you $PERSON2"
-echo "$PERSON2 : hi PERSON1 how are you"
+echo "$PERSON2 : hi $PERSON1 how are you"
 echo "$PERSON1 : I'm doing good $PERSON2"
 echo "$PERSON2 : how about BF $PERSON1"
 echo "$PERSON1 : Done $PERSON1, What about you"
