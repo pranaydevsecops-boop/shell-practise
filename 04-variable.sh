643f636299134b7f4f105866b9e319f7a9027e7e
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-29 10:01:21 +0530

    chnages

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1 : hi how are you $PERSON2"
+echo "$PERSON2 : hi PERSON1 how are you"
+echo "$PERSON1 : I'm doing good $PERSON2"
+echo "$PERSON2 : how about BF $PERSON1"
+echo "$PERSON1 : Done $PERSON1, What about you"
