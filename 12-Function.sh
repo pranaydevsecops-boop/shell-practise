0449eb474096411c042a76afd8765f9c4b6700c3
Pranay Reddy <pranaydevsecops@gmail.com>
2026-01-30 10:10:21 +0530

    new file added with changes

--- 12-Function.sh
+++ 12-Function.sh
@@ -9,3 +9,11 @@ fi
 
 echo "installing nginx"
 dnf install nginx -y
+
+STATUS=$?
+
+if[ $STATUS -ne 0 ]; then
+    echo "Installion is failed not successfull"
+else
+    echo "installion is pass adn sucessfull"
+fi
